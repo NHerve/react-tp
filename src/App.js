a87f67dc7150134565d433d0c56363c79ea957bf
NHerve <herve.04@hotmail.fr>
2020-02-10 11:01:11 +0100

    React01-introJSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const name = 'toto';
+  const avatar = 'path';
+  const online = true;
+  
+    return (
+        <div className="person-item">
+          <label>{online?'online':'offline'}</label>
+        </div>
+    );
 }
 
 export default App;
