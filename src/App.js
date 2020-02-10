5d828d8f3d6dbd49c988e240b01a5c171fbaa2c3
NHerve <herve.04@hotmail.fr>
2020-02-10 14:23:44 +0100

    creation de component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Contact from './Components/Contact'
 
-function App() {
+class App extends Component {
 
-  const test = {
-    name:'toto',
-    avatar : 'path',
-    online : true
+  state = {
+    ctx:[
+    {
+      id:1,
+      name:'toto',
+      avatar:"https://randomuser.me/api/portraits/thumb/men/75.jpg",
+      online:true
+    },
+    {
+      id:2,
+      name:'tata',
+      avatar:"https://randomuser.me/api/portraits/thumb/men/74.jpg",
+      online:false
+    }]
   }
-  
+
+  render(){
     return (
-        <div className="person-item">
-          <label>{test.online?'online':'offline'}</label>
+        <div>
+          <div className='person-item'>
+            <Contact contact={this.state.ctx[0]}/>
+          </div>
+          <div className='person-item'>
+            <Contact contact={this.state.ctx[1]}/>
+          </div>
         </div>
     );
+  }
 }
 
 export default App;
---
+++ src/Components/Contact.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <img src={this.props.contact.avatar}></img>
+                <label> {this.props.contact.name}</label>
+                <label> {this.props.contact.online?'online':'offline'}</label>
+            </div>
+        )
+    }
+}
+
+Contact.propTypes = {
+    contact: PropTypes.object.isRequired
+}
+
+
+
+
+export default Contact;
